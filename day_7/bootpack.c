df1bd9c35db5ea294f500545bda42f34ba4c7f7a
Akito Kasai <kasai@akito19.com>
2019-03-28 21:06:03 +0900

    harib04a

---
+++ day_7/bootpack.c
@@ -0,0 +1,32 @@
+// コンパイル時に -nostdlib を付与しているので，読み込めない．
+// #include <stdio.h>
+#include "bootpack.h"
+
+void HariMain(void)
+{
+    struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+    char s[40], mcursor[256];
+    int mx, my;
+
+    init_gdtidt();
+    init_pic();
+    io_sti(); // Set Interrupt Flag, 有効時，外部装置からの割り込みを受け付けるようになる．
+
+    init_palette();
+    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+
+    mx = (binfo->scrnx - 16) / 2;
+    my = (binfo->scrny - 28 - 16) / 2;
+
+    init_mouse_cursor(mcursor, COL8_008484);
+    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+    re_sprintf(s, "(%d, %d)", mx, my);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+    io_out8(PIC0_IMR, 0xf9);
+    io_out8(PIC1_IMR, 0xef);
+
+    for(;;) {
+        io_hlt;
+    }
+}
---
+++ day_7/int.c
@@ -0,0 +1,56 @@
+#include "bootpack.h"
+
+#define PORT_KEYDAT 0x0060
+
+// Initialize pic
+void init_pic(void)
+{
+    io_out8(PIC0_IMR,  0xff);   // すべての割り込みを受け付けない
+    io_out8(PIC1_IMR,  0xff);   // すべての割り込みを受け付けない
+
+    io_out8(PIC0_ICW1, 0x11);   // エッジトリガモード
+    io_out8(PIC0_ICW2, 0x20);   // IRQ0-7 は INT20-27 で受ける
+    io_out8(PIC0_ICW3, 1 << 2); // PIC1 は IRQ2 にて接続
+    io_out8(PIC0_ICW4, 0x01);   // ノンバッファモード
+
+    io_out8(PIC1_ICW1, 0x11);   // エッジトリガモード
+    io_out8(PIC1_ICW2, 0x11);   // IRQ8-15 は INT28-2f で受ける
+    io_out8(PIC1_ICW3, 0x11);   // PIC1 は IRQ2 にて接続
+    io_out8(PIC1_ICW4, 0x11);   // ノンバッファモード
+
+    io_out8(PIC0_IMR,  0xfb);   // 11111011 PIC1 以外はすべて禁止
+    io_out8(PIC1_IMR,  0xff);   // 11111111 すべての割り込みを受け付けない
+
+    return;
+}
+
+// PS/2キーボードからの割り込み
+void inthandler21(int *esp)
+{
+    struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+    unsigned char data, s[4];
+    io_out8(PIC0_OCW2, 0x61);  // IRQ-01受付完了PICに通知
+    data = io_in8(PORT_KEYDAT);
+
+    re_sprintf(s, "%x", data);
+    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+    return;
+}
+
+// PS/2マウスからの割り込み
+void inthandler2c(int *esp)
+{
+    struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+    boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
+    for (;;) {
+        io_hlt();
+    }
+}
+
+void inthandler27(int *esp)
+{
+    io_out8(PIC0_OCW2, 0x67);
+    return;
+}
