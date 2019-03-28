1d941724d029c38616d86781ed3239a7f25430a3
Akito Kasai <kasai@akito19.com>
2019-03-29 02:07:17 +0900

    harib04f

--- day_7/bootpack.c
+++ day_7/bootpack.c
@@ -1,8 +1,8 @@
-// コンパイル時に -nostdlib を付与しているので，読み込めない．
-// #include <stdio.h>
 #include "bootpack.h"
 
 struct FIFO8 keyfifo;
+void init_keyboard(void);
+void enable_mouse(void);
 
 void HariMain(void)
 {
@@ -13,21 +13,22 @@ void HariMain(void)
     init_gdtidt();
     init_pic();
     io_sti(); // Set Interrupt Flag, 有効時，外部装置からの割り込みを受け付けるようになる．
+    fifo8_init(&keyfifo, 32, keybuf);
+    io_out8(PIC0_IMR, 0xf9);
+    io_out8(PIC1_IMR, 0xef);
+
+    init_keyboard();
 
     init_palette();
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-    fifo8_init(&keyfifo, 32, keybuf);
-
     mx = (binfo->scrnx - 16) / 2;
     my = (binfo->scrny - 28 - 16) / 2;
-
     init_mouse_cursor(mcursor, COL8_008484);
     putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
     re_sprintf(s, "(%d, %d)", mx, my);
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-    io_out8(PIC0_IMR, 0xf9);
-    io_out8(PIC1_IMR, 0xef);
+    enable_mouse();
 
     for(;;) {
         io_cli();
@@ -41,4 +42,45 @@ void HariMain(void)
             putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16,  COL8_FFFFFF, s);
         }
     }
+}
+
+#define PORT_KEYDAT           0x0060
+#define PORT_KEYSTA           0x0064
+#define PORT_KEYCMD           0x0064
+#define KEYSTA_SEND_NOTREADY  0x02
+#define KEYCMD_WRITE_MODE     0x60
+#define KBC_MODE              0x47
+
+void wait_KBC_sendready(void)
+{
+    // keyboard controller がデータ送信可能になるのを待つ
+    for(;;) {
+        // 装置番号 0x0064 の下位2bitが0になるまで待つ
+        if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+            break;
+        }
+    }
+    return;
+}
+
+void init_keyboard(void)
+{
+    // Initialize keyboard controller
+    wait_KBC_sendready();
+    io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+    wait_KBC_sendready();
+    io_out8(PORT_KEYDAT, KBC_MODE);
+    return;
+}
+
+#define KEYCMD_SENDTO_MOUSE  0xd4
+#define MOUSECMD_ENABLE      0xf4
+
+void enable_mouse(void)
+{
+    wait_KBC_sendready();
+    io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+    wait_KBC_sendready();
+    io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+    return; // うまくいくとACK(0xfa)が送信されてくる
 }
