39de43b490a2cfeaa50911f6beb476d9736efda9
Akito Kasai <kasai@akito19.com>
2019-03-30 20:19:20 +0900

    harib05a

--- day_8/bootpack.c
+++ day_8/bootpack.c
@@ -9,6 +9,7 @@ void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
     char s[40], mcursor[256], keybuf[32], mousebuf[128];
+    unsigned char mouse_dbuf[3], mouse_phase;
     int i, mx, my;
 
     init_gdtidt();
@@ -31,6 +32,7 @@ void HariMain(void)
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
     enable_mouse();
+    mouse_phase = 0; // マウスの 0xfa を待っている段階
 
     for(;;) {
         io_cli();
@@ -46,9 +48,27 @@ void HariMain(void)
             } else if (fifo8_status(&mousefifo) != 0) {
                 i = fifo8_get(&mousefifo);
                 io_sti();
-                re_sprintf(s, "%x", i);
-                boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-                putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                if (mouse_phase == 0) {
+                    // マウスの 0xfa を待っている段階
+                    if (i == 0xfa) {
+                        mouse_phase = 1;
+                    }
+                } else if (mouse_phase == 1) {
+                    // マウスの1バイト目を待っている段階
+                    mouse_dbuf[0] = i;
+                    mouse_phase = 2;
+                } else if (mouse_phase == 2) {
+                    // 2バイト目ry
+                    mouse_dbuf[1] = i;
+                    mouse_phase = 3;
+                } else if (mouse_phase == 3) {
+                    mouse_dbuf[2] = i;
+                    mouse_phase = 1;
+                    // データが3バイト揃ったので表示
+                    re_sprintf(s, "%x %x %x", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+                    putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                }
             }
         }
     }
