d4c64e96d05ebaf7c51dc6fa81909b2506c62cc0
Akito Kasai <kasai@akito19.com>
2019-04-20 20:58:09 +0900

    harib14c

--- day_17/bootpack.c
+++ day_17/bootpack.c
@@ -2,6 +2,7 @@
 
 void console_task(struct SHEET *sheet);
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
 void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
 void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 
@@ -20,6 +21,7 @@ void HariMain(void)
     char s[40];
     int fifobuf[128];
     int mx, my, i, cursor_x, cursor_c;
+    int key_to = 0;
     unsigned int memtotal;
     static char keytable[0x54] = {
         0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
@@ -131,6 +133,19 @@ void HariMain(void)
                     putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1);
                     cursor_x -= 8;
                 }
+                if (i == 256 + 0x0f) { // tab
+                    if (key_to == 0) {
+                        key_to = 1;
+                        make_wtitle8(buf_win, sht_win->bxsize, "task_a", 0);
+                        make_wtitle8(buf_cons, sht_cons->bxsize, "console", 1);
+                    } else {
+                        key_to = 0;
+                        make_wtitle8(buf_win, sht_win->bxsize, "task_a", 1);
+                        make_wtitle8(buf_cons, sht_cons->bxsize, "console", 0);
+                    }
+                    sheet_refresh(sht_win, 0, 0, sht_win->bxsize, 21);
+                    sheet_refresh(sht_cons, 0, 0, sht_win->bxsize, 21);
+                }
                 // カーソルの再表示
                 boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
                 sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
@@ -225,6 +240,21 @@ void console_task(struct SHEET *sheet)
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act)
 {
+    boxfill8(buf, xsize, COL8_C6C6C6,         0,         0, xsize - 1,         0);
+    boxfill8(buf, xsize, COL8_FFFFFF,         1,         1, xsize - 2,         1);
+    boxfill8(buf, xsize, COL8_C6C6C6,         0,         0,         0, ysize - 1);
+    boxfill8(buf, xsize, COL8_FFFFFF,         1,         1,         1, ysize - 2);
+    boxfill8(buf, xsize, COL8_848484, xsize - 2,         0, xsize - 2, ysize - 2);
+    boxfill8(buf, xsize, COL8_000000, xsize - 1,         0, xsize - 1, ysize - 1);
+    boxfill8(buf, xsize, COL8_C6C6C6,         2,         0, xsize - 3, ysize - 3);
+    boxfill8(buf, xsize, COL8_848484,         1, ysize - 2, xsize - 2, ysize - 2);
+    boxfill8(buf, xsize, COL8_000000,         0, ysize - 1, xsize - 1, ysize - 1);
+    make_wtitle8(buf, xsize, title, act);
+    return;
+}
+
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act)
+{
     static char closebtn[14][16] = {
         "OOOOOOOOOOOOOOO@",
         "OQQQQQQQQQQQQQ$@",
@@ -251,19 +281,7 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char ac
         tc = COL8_C6C6C6;
         tbc = COL8_848484;
     }
-    boxfill8(buf, xsize, COL8_C6C6C6,         0,         0, xsize - 1,         0);
-    boxfill8(buf, xsize, COL8_FFFFFF,         1,         1, xsize - 2,         1);
-    boxfill8(buf, xsize, COL8_C6C6C6,         0,         0,         0, ysize - 1);
-    boxfill8(buf, xsize, COL8_FFFFFF,         1,         1,         1, ysize - 2);
-    boxfill8(buf, xsize, COL8_848484, xsize - 2,         0, xsize - 2, ysize - 2);
-    boxfill8(buf, xsize, COL8_000000, xsize - 1,         0, xsize - 1, ysize - 1);
-    boxfill8(buf, xsize, COL8_C6C6C6,         2,         0, xsize - 3, ysize - 3);
-    boxfill8(buf, xsize, COL8_000084,         3,         0, xsize - 4,        20);
-    boxfill8(buf, xsize, COL8_848484,         1, ysize - 2, xsize - 2, ysize - 2);
-    boxfill8(buf, xsize, COL8_000000,         0, ysize - 1, xsize - 1, ysize - 1);
-    boxfill8(buf, xsize,         tbc,         3,         3, xsize - 4,        20);
-    boxfill8(buf, xsize, COL8_848484,         1, ysize - 2, xsize - 2, ysize - 2);
-    boxfill8(buf, xsize, COL8_000000,         0, ysize - 1, xsize - 1, ysize - 1);
+    boxfill8(buf, xsize, tbc, 3, 3, xsize - 4, 20);
     putfonts8_asc(buf, xsize, 24, 4, tc, title);
     for (y = 0; y < 14; y++) {
         for (x = 0; x < 16; x++) {
