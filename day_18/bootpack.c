8557e552f943d61aecff385aaa4e18455bf383a7
Akito Kasai <kasai@akito19.com>
2019-04-21 14:37:40 +0900

    harib15c

--- day_18/bootpack.c
+++ day_18/bootpack.c
@@ -180,6 +180,11 @@ void HariMain(void)
                         fifo32_put(&task_cons->fifo, 8 + 256);
                     }
                 }
+                if (i == 256 + 0x1c) { // Enter
+                    if (key_to != 0) { // コンソールへ
+                        fifo32_put(&task_cons->fifo, 10 + 256);
+                    }
+                }
                 if (i == 256 + 0x0f) { // tab
                     if (key_to == 0) {
                         key_to = 1;
@@ -299,7 +304,7 @@ void console_task(struct SHEET *sheet)
 {
     struct TIMER *timer;
     struct TASK *task = task_now();
-    int i, fifobuf[128], cursor_x = 16, cursor_c = -1;
+    int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
     char s[2];
 
     fifo32_init(&task->fifo, 128, fifobuf, task);
@@ -343,21 +348,31 @@ void console_task(struct SHEET *sheet)
                 if (i == 8 + 256) {
                     // backspace
                     if (cursor_x > 16) {
-                        putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, " ", 1);
+                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
                         cursor_x -= 8;
+                    }
+                } else if (i == 10 + 256) {
+                    // Enter
+                    if (cursor_y < 28 + 112) {
+                        // カーソルをスペースで消す
+                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
+                        cursor_y += 16;
+                        // プロンプト表示
+                        putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, "$", 1);
+                        cursor_x = 16;
                     }
                 } else {
                     // 一般文字
                     if (cursor_x < 240) {
                         s[0] = i - 256;
                         s[1] = 0;
-                        putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, s, 1);
+                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
                         cursor_x += 8;
                     }
                 }
             }
             if (cursor_c >= 0) {
-                boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+                boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
             }
             sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
         }
