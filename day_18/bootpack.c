7f9a68fb0731f347ccd1c50be09117e6a10bffac
Akito Kasai <kasai@akito19.com>
2019-04-21 16:49:34 +0900

    harib15e

--- day_18/bootpack.c
+++ day_18/bootpack.c
@@ -2,7 +2,8 @@
 
 #define KEYCMD_LED  0xed
 
-void console_task(struct SHEET *sheet);
+void console_task(struct SHEET *sheet, unsigned int memtotal);
+int cons_newline(int cursor_y, struct SHEET *sheet);
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
 void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
 void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
@@ -80,7 +81,7 @@ void HariMain(void)
     make_window8(buf_cons, 256, 165, "console", 0);
     make_textbox8(sht_cons, 8, 28, 240, 128, COL8_000000);
     task_cons = task_alloc();
-    task_cons->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 8;
+    task_cons->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
     task_cons->tss.eip = (int) &console_task;
     task_cons->tss.es = 1 * 8;
     task_cons->tss.cs = 2 * 8;
@@ -89,6 +90,7 @@ void HariMain(void)
     task_cons->tss.fs = 1 * 8;
     task_cons->tss.gs = 1 * 8;
     *((int *) (task_cons->tss.esp + 4)) = (int) sht_cons;
+    *((int *) (task_cons->tss.esp + 8)) = memtotal;
     task_run(task_cons, 2, 2);  // level=2, priority=2
 
     // sht_win
@@ -119,10 +121,6 @@ void HariMain(void)
     sheet_updown(sht_cons,  1);
     sheet_updown(sht_win,   2);
     sheet_updown(sht_mouse, 3);
-    mysprintf(s, "(%d, %d)", mx, my);
-    putfonts8_asc_sht(sht_back, 0, 0, COL8_FFFFFF, COL8_008484, s, 10);
-    mysprintf(s, "memory %d MB   free: %d KB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
-    putfonts8_asc_sht(sht_back, 0, 32, COL8_FFFFFF, COL8_008484, s, 40);
 
     // 最初にキーボード状態との食い違いがないように，設定しておく
     fifo32_put(&keycmd, KEYCMD_LED);
@@ -143,8 +141,6 @@ void HariMain(void)
             i = fifo32_get(&fifo);
             io_sti();
             if (256 <= i && i <= 511) { // Keyboard data
-                mysprintf(s, "%X", i - 256);
-                putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
                 if (i < 0x80 + 256) { // キーコードを文字コードに変換
                     if (key_shift == 0) {
                         s[0] = keytable0[i - 256];
@@ -244,18 +240,6 @@ void HariMain(void)
                 sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
             } else if (512 <= i && i <= 767) { // Mouse data
                 if (mouse_decode(&mdec, i - 512) != 0) {
-                    // データが3バイト揃ったので表示
-                    mysprintf(s, "[lcr %d %d]", mdec.x, mdec.y);
-                    if ((mdec.btn & 0x01) != 0) {
-                        s[1] = 'L';
-                    }
-                    if ((mdec.btn & 0x02) != 0) {
-                        s[3] = 'R';
-                    }
-                    if ((mdec.btn & 0x04) != 0) {
-                        s[2] = 'C';
-                    }
-                    putfonts8_asc_sht(sht_back, 32, 16, COL8_FFFFFF, COL8_008484, s, 15);
                     // マウスカーソルの移動
                     mx += mdec.x;
                     my += mdec.y;
@@ -271,8 +255,6 @@ void HariMain(void)
                     if (my > binfo->scrny - 1) {
                         my = binfo->scrny - 1;
                     }
-                    mysprintf(s, "(%d, %d)", mx, my);
-                    putfonts8_asc_sht(sht_back, 0, 0, COL8_FFFFFF, COL8_008484, s, 10);
                     sheet_slide(sht_mouse, mx, my);
                     if ((mdec.btn & 0x01) != 0) {
                         sheet_slide(sht_win, mx - 80, my - 8);
@@ -300,13 +282,14 @@ void HariMain(void)
     }
 }
 
-void console_task(struct SHEET *sheet)
+void console_task(struct SHEET *sheet, unsigned int memtotal)
 {
     struct TIMER *timer;
     struct TASK *task = task_now();
+    struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
     int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
     int x, y;
-    char s[2];
+    char s[30], cmdline[30];
 
     fifo32_init(&task->fifo, 128, fifobuf, task);
     timer = timer_alloc();
@@ -355,22 +338,23 @@ void console_task(struct SHEET *sheet)
                 } else if (i == 10 + 256) {
                     // Enter
                     putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
-                    if (cursor_y < 28 + 112) {
-                        // カーソルをスペースで消す
-                        cursor_y += 16; // 次の行へ
-                    } else {
-                        // スクロール
-                        for (y = 28; y < 28 + 112; y++) {
-                            for (x = 8; x < 8 + 240; x++) {
-                                sheet->buf[x + y * sheet->bxsize] = sheet->buf[x + (y + 16) * sheet->bxsize];
-                            }
-                        }
-                        for (y = 28 + 112; y < 28 + 128; y++) {
-                            for (x = 8; x < 8 + 240; x++) {
-                                sheet->buf[x + y * sheet->bxsize] = COL8_000000;
-                            }
-                        }
-                        sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+                    cmdline[cursor_x / 8 - 2] = 0;
+                    cursor_y = cons_newline(cursor_y, sheet);
+                    // コマンド実行
+                    if (cmdline[0] == 'm' && cmdline[1] == 'e' && cmdline[2] == 'm' && cmdline[3] == 0) {
+                        // `mem` command
+                        mysprintf(s, "total %dMB", memtotal / (1024 * 1024));
+                        putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, s, 30);
+                        cursor_y = cons_newline(cursor_y, sheet);
+                        mysprintf(s, "free %dKB", memman_total(memman) / 1024);
+                        putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, s, 30);
+                        cursor_y = cons_newline(cursor_y, sheet);
+                        cursor_y = cons_newline(cursor_y, sheet);
+                    } else if (cmdline[0] != 0) {
+                        // コマンドではなく，改行でもない
+                        putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, "Bad command.", 12);
+                        cursor_y = cons_newline(cursor_y, sheet);
+                        cursor_y = cons_newline(cursor_y, sheet);
                     }
                     // プロンプト表示
                     putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, "$", 1);
@@ -380,6 +364,7 @@ void console_task(struct SHEET *sheet)
                     if (cursor_x < 240) {
                         s[0] = i - 256;
                         s[1] = 0;
+                        cmdline[cursor_x / 8 - 2] = i - 256;
                         putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
                         cursor_x += 8;
                     }
@@ -391,6 +376,27 @@ void console_task(struct SHEET *sheet)
             sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
         }
     }
+}
+
+int cons_newline(int cursor_y, struct SHEET *sheet)
+{
+    int x, y;
+    if (cursor_y < 28 + 112) {
+        cursor_y += 16; // 次の行へ
+    } else {
+        for (y = 28; y < 28 + 112; y++) {
+            for (x = 8; x < 8 + 240; x++) {
+                sheet->buf[x + y * sheet->bxsize] = sheet->buf[x + (y + 16) * sheet->bxsize];
+            }
+        }
+        for (y = 28 + 112; y < 28 + 128; y++) {
+            for (x = 8; x < 8 + 240; x++) {
+                sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+            }
+        }
+        sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+    }
+    return cursor_y;
 }
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act)
