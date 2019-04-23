05b73cca1de798e6db9e484b8f62ead6ed1c11f7
Akito Kasai <kasai@akito19.com>
2019-04-23 21:50:24 +0900

    harib19g

--- day_22/app/api.asm
+++ day_22/app/api.asm
@@ -4,6 +4,8 @@ GLOBAL api_putchar
 GLOBAL api_end
 GLOBAL api_putstr0
 GLOBAL api_openwin
+GLOBAL api_putstrwin
+GLOBAL api_boxfilwin
 
 section .text
 
@@ -37,6 +39,44 @@ api_openwin:
 	mov   ecx,[esp+32] ; title
 	int   0x40
 	pop   ebx
+	pop   esi
+	pop   edi
+	ret
+
+api_putstrwin:
+	push  edi
+	push  esi
+	push  ebp
+	push  ebx
+	mov   edx,6
+	mov   ebx,[esp+20] ; win
+	mov   esi,[esp+24] ; x
+	mov   edi,[esp+28] ; y
+	mov   eax,[esp+32] ; col
+	mov   ecx,[esp+36] ; len
+	mov   ebp,[esp+40] ; str
+	int   0x40
+	pop   ebx
+	pop   ebp
+	pop   esi
+	pop   edi
+	ret
+
+api_boxfilwin:
+	push  edi
+	push  esi
+	push  ebp
+	push  ebx
+	mov   edx,7
+	mov   ebx,[esp+20] ; win
+	mov   eax,[esp+24] ; x0
+	mov   ecx,[esp+28] ; y0
+	mov   esi,[esp+32] ; x1
+	mov   edi,[esp+36] ; y1
+	mov   ebp,[esp+40] ; col
+	int   0x40
+	pop   ebx
+	pop   ebp
 	pop   esi
 	pop   edi
 	ret
